1bbda1788eed46f498785225ed2fdda000497239
sas03 <stephanesumo@yahoo.fr>
2023-07-24 10:12:54 +0200

    Enumeration

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using Enumeration;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        List<User> userList = new List<User>
+        {
+            new User("Doe", "John", Color.ROUGE),
+            new User("Landt", "May", Color.JAUNE),
+            new User("Madrickson", "Maurice", Color.ROUGE),
+            new User("Yonos", "Jack", Color.VERT),
+            new User("Parker", "Tony", Color.VERT),
+            new User("Dolodo", "Lucie", Color.BLEU),
+            new User("Antack", "Fulis", Color.VERT)
+        };
+
+        foreach(User user in userList)
+        {
+            Console.WriteLine("Prénom : " + user.Prenom);
+            Console.WriteLine("Nom : " + user.Nom);
+            Console.WriteLine("Couleur : " + user.Couleur);
+        }
+
+        int countRouge = userList.FindAll(u => u.Couleur == Color.ROUGE).Count();
+        int countVert = userList.FindAll(u => u.Couleur == Color.VERT).Count();
+        int countJaune = userList.FindAll(u => u.Couleur == Color.JAUNE).Count();
+        int countBleu = userList.FindAll(u => u.Couleur == Color.BLEU).Count();
+
+        Console.WriteLine("ROUGE : " + countRouge + " Utilisateurs");
+        Console.WriteLine("VERT : " + countVert + " Utilisateurs");
+        Console.WriteLine("JAUNE : " + countJaune + " Utilisateurs");
+        Console.WriteLine("BLEU : " + countBleu + " Utilisateurs");
+    }
+}
---
+++ User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enumeration
+{
+    public class User
+    {
+        public string Nom { get; set; }
+        public string Prenom { get; set; }
+        public Color Couleur { get; set; }
+        public User(string nom, string prenom, Color couleur)
+        {
+            Nom = nom;
+            Prenom = prenom;
+            Couleur = couleur;
+        }
+    }
+}
